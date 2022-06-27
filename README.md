706ac2b6bc0867c19180656a6c3cb8fffd10e215
Pablety <pablog.con@gmail.com>
2022-06-27 13:47:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vuepic-vue-datepicker-dxsqq9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vuepic-vue-datepicker-dxsqq9)
---
+++ src/components/Playground.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h5>{{ date }}</h5>
+    <hr />
+    <Datepicker
+      v-model="date"
+      inline
+      autoApply
+      inlineWithInput
+      :enableTimePicker="false"
+      dark
+      inputClassName="dp-custom-input"
+      :allowedDates="allowedDates"
+      locale="latam"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue';
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
+//allowedDates = computed(new Date())
+
+const date = ref();
+
+// For demo purposes, enable only today and tomorrow
+const allowedDates = computed(() => {
+  return [new Date(), new Date(new Date().setDate(new Date().getDate() + 1))];
+});
+</script>
